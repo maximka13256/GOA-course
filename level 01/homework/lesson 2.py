e82fac5c2650964ef4ba305b1bd8031332299364
maximka13256 <novikovmaxim13256@gmail.com>
2025-02-15 11:08:07 -0800

    commit

---
+++ level 01/homework/lesson 2.py
@@ -0,0 +1,31 @@
+name = "maxim"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+#"maxim" არის ცვლადისთვის მნიშვნელობა
+
+surname = "ნოვიკოვი"
+
+# print(name)
+#პრინტ ფუნქცია გადაეცემა ეკრანზე ობიექტი
+
+name = "მაქსიმი" #ეს არის str (string) ტიპის ცვლადი
+age = 13 # ეს არის int ( integer ) მთელი რიცხვი 
+heigh = 155.8 #ეს არის float ტიპის ცვლადი ( ათწილადი )
+#booLean (booL) ტიპის ცვლადი
+
+knows_programming = True   #True ან False
+is_Cool = True  #snakecase( უბრალოდ წერის სტილი სტანდარტულად )
+isCool = True # ჯავასკრიპტული cameLcase
+# print(name = " " + surname)
+# #სტრინგი არის ბრჭყალებში მოქცეუკი სიმბოლოებში
+# print(name + age)
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(heigh))
+# print(type(knows_programming))
+
+print("მე ვარ" + " " + name + " " + "ჩემი გვარი არის" + " " + surname + " " + str(age) + " " + "წლის ვარ" + " " + "და მიყვარს თამაშები და მანქანები!")
+
+# მე ვარ მაქსიმ, ჩემი გვარი არის ნოვიკოვ. 13 წლის ვარ და მიყვარს თამაშები და მანქანები!
