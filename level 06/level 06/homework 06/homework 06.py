08acb9d71ebf2115e9037c1df98961dfe683c3e6
maximka13256 <novikovmaxim13256@gmail.com>
2025-02-26 09:00:44 -0800

    commit

---
+++ level 06/level 06/homework 06/homework 06.py
@@ -0,0 +1,15 @@
+name = "maxim"
+name = "luka"
+print(name)
+name = "vano"
+name = "nika"
+print(name)
+name = "irakli"
+name = "ronaldo"
+print(name)
+name = "saba"
+name = "nodar"
+print(name)
+name = "zaza"
+name = "mamuka"
+print(name)
