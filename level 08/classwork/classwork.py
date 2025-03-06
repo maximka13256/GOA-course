2a794c96acf6c187f61dfc14dd515bfa32fd5526
maximka13256 <novikovmaxim13256@gmail.com>
2025-03-06 09:48:08 -0800

    commit

---
+++ level 08/classwork/classwork.py
@@ -0,0 +1,11 @@
+# შვიდი იქნება იმიტომ რომ არის ორი ნეიმი დიდ ასოების გარეშე. 
+my_favorite_car = "audi"
+my_name = "maxim"
+dog_name = "bobik"
+my_favorite_brand = "samsung"
+who_i_love = "mom and dad"
+print(my_favorite_car)
+print(my_name)
+print(dog_name)
+print(my_favorite_brand)
+print(who_i_love)
