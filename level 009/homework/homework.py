8699b6d27ac1026e035f0b93996da7d143b06c6c
maximka13256 <novikovmaxim13256@gmail.com>
2025-03-16 09:42:13 -0700

    commit

---
+++ level 009/homework/homework.py
@@ -0,0 +1,18 @@
+# ურთიერთქმედება ინფორმაციის პროცესორს (მაგალითად, კომპიუტერს)
+# და გარე სამყაროს შორის, რომელიც შეიძლება იყოს წარმოდგენილი როგორც
+# პირის (სუბიექტის) და ინფორმაციის დამუშავების ნებისმიერი სხვა სისტემის მიერ.
+my_friend_name = "nika"
+my_friend_surname = "odzelashvili"
+print(my_friend_name)
+print(my_friend_surname)
+num1 = input("ნების მიერი რიცხვი")
+num2 = input("ნების მიერი რიცხვი")
+num3 = input("ნების მიერი რიცხვი")
+num4 = input("ნების მიერი რიცხვი")
+num5 = input("ნების მიერი რიცხვი")
+print(input(num1+num2+num3+num4+num5))
+name = "nika"
+surname = "odzelashvili"
+age = "12"
+height = "***"
+weight = "**"
