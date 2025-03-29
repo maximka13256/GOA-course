7f4cc5fdc5403ce314457e751254e409c8774446
maximka13256 <novikovmaxim13256@gmail.com>
2025-03-28 23:39:13 -0700

    commit

--- level 002/homework/homework4.py
+++ level 002/homework/homework4.py
@@ -1,2 +1,17 @@
+from turtle import *
+speed(20)
+width(7)
+color("black")
+forward(100)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(100)
+exitonclick()
 name = "maxim"
 print(name)
+# ცვლადები გამოიყენება მონაცემების შესანახად
