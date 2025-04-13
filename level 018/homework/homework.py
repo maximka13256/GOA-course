cf8323a7175c576a99acbbf083795535c828b1fd
maximka13256 <novikovmaxim13256@gmail.com>
2025-04-13 17:37:11 +0400

    commit

---
+++ level 018/homework/homework.py
@@ -0,0 +1,16 @@
+name = input("hello, write your name please:")
+if name == ("nika"):
+    print("in georgia a lot off ppl is named nika")
+else:
+    print("your name is rare")
+    if name == ("nika"):
+        rare = "no"
+        print(rare)
+        score = 68
+        exam = "failed"
+        print(exam)
+        grade = "b"
+        print(grade)
+# პითონის პროგრამირების ენაზე დაწერილი ინსტრუქციების ნაკრები,
+# რომელიც განსაზღვრავს ოპერაციების შესრულების თანმიმდევრობას კონკრეტული პრობლემის გადასაჭრელად.
+        
