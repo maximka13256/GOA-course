bb8b020e65f6e025a1a112ef0e5ef41ac7f82ba6
maximka13256 <novikovmaxim13256@gmail.com>
2025-04-25 21:48:52 +0400

    commit

---
+++ level 022/homework/homework.py
@@ -0,0 +1,17 @@
+# დიაპაზონის ფუნქცია პითონში გამოიყენება რიცხვების თანმიმდევრობის შესაქმნელად.
+# ის აბრუნებს ობიექტს, რომელიც შეიძლება გამოყენებულ იქნას მარყუჟებში და სხვა კონსტრუქტებში,
+# რომლებიც საჭიროებენ გამეორებას.
+for i in range(0, 120, 20):
+    print(i)
+    num = int(input("Please enter a number: "))
+
+if 25 % 3 == 1:
+    print("The number is even")
+else:
+    print("The number is odd")
+    my_option = 23
+    the_number_to_divide_by = 4
+    print(my_option / the_number_to_divide_by)
+    print(my_option // the_number_to_divide_by)
+    print(my_option % the_number_to_divide_by)
+    # bolos mere gavaketeb
