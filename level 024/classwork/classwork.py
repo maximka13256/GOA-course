8aefa38d431fee0a923f5cf50388ad84c9b81c47
maximka13256 <novikovmaxim13256@gmail.com>
2025-04-30 19:11:37 +0400

    commit

---
+++ level 024/classwork/classwork.py
@@ -0,0 +1,14 @@
+number = 1
+while number <= 10:
+    print(number)
+    number = number + 1
+
+    number = 1
+while number <= 100:
+    print(number)
+    number = number + 1
+
+    number = 0
+while number <= 20:
+    print(number)
+    number = number + 2
